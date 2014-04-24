5d2950e2ccf92eb3e27c3af23be36f4a15be5c3c
Jiaxin Shan <seedjeffwan@gmail.com>
2014-04-24 01:06:31 -0700

    1.Finish Login, Logout, Registration and Posts save and insert Operations. 2.Add Post markdown support q :

---
+++ models/post.js
@@ -0,0 +1,89 @@
+var mongodb = require('./db');
+markdown = require('markdown').markdown;
+
+function Post(username, title, post) {
+    this.username = username;
+    this.title = title;
+    this.post = post;
+}
+
+
+module.exports = Post;
+
+Post.prototype.save = function(callback) {
+    var date = new Date();
+    var time = {
+        date: date,
+        year: date.getFullYear(),
+        month: date.getFullYear() + '-' +(date.getMonth() - 1),
+        day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+        minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+            date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+
+    };
+
+    var post = {
+        username: this.username,
+        time: time,
+        title: this.title,
+        post: this.post
+    }
+
+    mongodb.open(function(err, db) {
+        if (err) {
+            return callback(err);
+        }
+
+        // read post collections
+        db.collection('posts', function(err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+
+            collection.insert(post, {safe: true} , function(err, post) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null, post[0]);
+            });
+        });
+    });
+};
+
+Post.get = function(username, callback) {
+    // Open db
+    mongodb.open(function(err, db) {
+        if (err) {
+            return callback(err);
+        }
+
+        db.collection('posts', function(err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+
+            var query = {};
+            if (username) {
+                query.username = username;
+            }
+
+            collection.find(query).sort({
+                time:-1
+            }).toArray(function(err, docs) {
+                    mongodb.close();
+                    if (err) {
+                        return callback(err);  // fail
+                    }
+
+                    docs.forEach(function(doc) {
+                        doc.post = markdown.toHTML(doc.post);
+                    })
+
+                    callback(null, docs); // success
+             });
+        });
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var crypto = require('crypto');
 var User = require('../models/user.js');
+var Post = require('../models/post.js');
 
 var express = require('express');
 var router = express.Router();
@@ -8,18 +9,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', {
+    Post.get(null, function(err, posts) {
+        if (err) {
+            posts = [];
+        }
 
-      title: 'Express',
-      user: req.session.user,
-      success: req.flash('success').toString(),
-      error: req.flash('error').toString()
+        res.render('index', {
+            title: 'Home',
+            user: req.session.user,
+            posts: posts,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
+    });
 
-
-  });
 });
 
-
+router.get('/reg', checkNotLogin);
 router.get('/reg', function(req, res) {
     res.render('reg', {
         title : 'Registration',
@@ -30,6 +36,7 @@ router.get('/reg', function(req, res) {
     });
 });
 
+router.post('/reg', checkNotLogin);
 router.post('/reg', function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
@@ -75,25 +82,90 @@ router.post('/reg', function(req, res) {
     });
 });
 
+router.get('/login', checkNotLogin);
 router.get('/login', function(req, res) {
-    res.render('login', { title : 'Login'});
+    res.render('login', {
+        title : 'Login',
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+    });
 });
 
+router.post('/login', checkNotLogin);
 router.post('/login', function(req, res) {
+    var md5 = crypto.createHash('md5'),
+        password = md5.update(req.body.password).digest('hex');
 
+    User.get(req.body.username, function(err, user) {
+        if (!user) {
+            req.flash('error', 'user not exist');
+            return res.redirect('/login');
+        }
+
+        if (user.password != password) {
+            req.flash('error', 'password not right!');
+            return res.redirect('/login');
+        }
+
+        req.session.user = user;
+        req.flash('success', 'login successfully!');
+        res.redirect('/');
+
+    });
 });
 
+router.get('/post', checkLogin);
 router.get('/post', function(req, res) {
-    res.render('index', { title : 'Post Blog'});
+    res.render('post', {
+        title : 'Post',
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+    });
 });
 
+
+router.post('/post', checkLogin);
 router.post('/post', function(req, res) {
-
+    var currentUser = req.session.user;
+    var post = new Post(currentUser.username, req.body.title, req.body.post);
+    post.save(function(err) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('/');
+        }
+        req.flash('success', 'post successfully!');
+        res.redirect('/');
+    });
 });
 
+router.get('/logout', checkLogin);
 router.get('/logout', function(req, res) {
-    res.render('index', { title : 'logout'});
+    req.session.user = null;
+    req.flash('success', 'logout successfully!');
+    res.redirect('/');
 });
 
 
 module.exports = router;
+
+
+// 这部分用4.0 的新的表达方式可以重构掉？
+function checkLogin(req, res, next) {
+    if (!req.session.user) {
+        req.flash('error', 'Need to login first');
+        res.redirect('/login');
+    }
+    next();
+}
+
+function checkNotLogin(req, res, next) {
+    if (req.session.user) {
+        req.flash('error', 'You cannot do that with login status');
+        res.redirect('back');
+    }
+    next();
+}
+
+
